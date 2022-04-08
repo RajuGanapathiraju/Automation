43b1e607f46306a68c6f6053bcab962eba8c3cb5
Raju GY <rajugy@raju-macbook-pro.bbrouter>
2022-04-08 11:21:48 +0530

    second commit

---
+++ external_shared_file_automation.js
@@ -0,0 +1,134 @@
+var request = require('request');
+var async = require('async');
+var unique = require('unique-array');
+var hdate = require('human-date')
+const config = require('dotenv').config({
+    path: '/Users/rajugy/Documents/scripts/.env'
+});
+
+let alarm_array = [];
+
+let individual_alarm_array = [];
+
+let final_alarms = [];
+
+let username_array = [];
+
+let token;
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'kickraju';
+
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    start(db);
+});
+
+
+function start(db) {
+    function extract(final_alarms) {
+        async.forEach(final_alarms, (item, cb) => {
+            console.log(item.message.event_name + "--->>" + item.message.file_type + "--->>" + item.message.file_owner + "--->>" + item.message.timestamp_received + "--->>" + item.message.source_username + "--->>" + item.message.destination_username + "--->>" + item.message.file_name);
+            db.collection('alarms').insert({event_name: item.message.event_name, file_type: item.message.file_type, file_owner: item.message.file_owner, timestamp: hdate.prettyPrint(new Date(parseInt(item.message.timestamp_received)), { showTime: true }), source_username: item.message.source_username, destination_username: item.message.destination_username, file_name: item.message.file_name}, (err, result) => {
+              cb();  
+            })
+        }, (err, res) => {
+            console.log('=========================================')
+        })
+    }
+
+    function individualFilterOut(alarm_array) {
+        async.forEach(alarm_array, (item, cb) => {
+            async.forEach(item.events, (sub_item, cb1) => {
+                final_alarms.push(sub_item);
+                cb1();
+            }, (err, res) => {
+                cb()
+            })
+
+        }, (err, res) => {
+            extract(final_alarms);
+        })
+    }
+
+
+    function filterOut(alarms_data) {
+        async.forEach(alarms_data, (alarm, cb) => {
+            if (alarm.destination_username.indexOf('@insideview.com') == -1 && alarm.destination_username.indexOf('@demandbase.com') == -1) {
+                alarm_array.push(alarm);
+                cb();
+            } else {
+                cb();
+            }
+        }, (err, res) => {
+            individualFilterOut(alarm_array);
+        })
+    }
+
+
+
+    let promise = new Promise((resolve, reject) => {
+
+        var dataString = '@request_body';
+
+        var options = {
+            url: 'https://insideview.alienvault.cloud/api/2.0/oauth/token?grant_type=client_credentials',
+            method: 'POST',
+            body: dataString,
+            auth: {
+                'user': process.env.API_USERNAME,
+                'pass': process.env.API_PASSWORD
+            }
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                let result = JSON.parse(body);
+                token = result.access_token;
+                console.log(result.access_token);
+                resolve(result.access_token);
+            } else {
+                reject("error at generating access token");
+            }
+        }
+
+        request(options, callback);
+
+    })
+
+
+    promise.then((accessToken) => {
+
+        var headers = {
+            'Authorization': `Bearer ${accessToken}`
+        };
+
+        var options = {
+            url: `https://insideview.alienvault.cloud/api/2.0/alarms?size=1000&status=open&rule_intent=Environmental%20Awareness&rule_method=File%20Shared%20Externally&rule_strategy=Filesharing%20outside%20IV%20over%20email&priority_label=low&alarm_sensor_sources=cf60463c-de16-45ff-946e-627aecbb53d9&timestamp_occured_gte=${parseInt(Date.now())-86400000}&timestamp_occured_lte=${Date.now()}`,
+            headers: headers
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                let alarms_data = JSON.parse(body)._embedded.alarms;
+                filterOut(alarms_data);
+            } else {
+                console.log("Error at getting alarms data");
+            }
+        }
+
+        request(options, callback);
+
+
+    }).catch((error) => {
+        console.log(error);
+    })
+
+}
---
+++ sendEmails.js
@@ -0,0 +1,110 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const async = require('async');
+const ObjectsToCsv = require('objects-to-csv');
+
+const nodemailer = require('nodemailer');
+var sesTransport = require('nodemailer-ses-transport');
+var smtpPassword = require('aws-smtp-credentials');
+const config = require('dotenv').config({ path: '/Users/rajugy/Documents/scripts/.env' })
+
+// let mailTransporter = nodemailer.createTransport({	
+// 	service: 'gmail',
+// 	auth: {
+// 		user: process.env.GMAIL_USERNAME,
+// 		pass: process.env.GMAIL_PASSWORD
+// 	}
+// });
+
+// let mailTransporter = nodemailer.createTransport(sesTransport({
+//   accessKeyId: process.env.accessKeyId,
+//   secretAccessKey: process.env.accessKey
+// }));
+			
+var mailTransporter = nodemailer.createTransport({
+  port: 465,
+  host: 'email-smtp.us-east-1.amazonaws.com',
+  secure: true,
+  auth: {
+    user: process.env.accessKeyId,
+    pass: smtpPassword(process.env.accessKey),
+  },
+  debug: true
+});
+
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'kickraju';
+
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    start(db);
+});
+
+function start(db) {
+    db.collection('alarms').distinct('source_username', {sent: null}, (err, users) => {
+        async.eachSeries(users, (item, cb) => {
+            db.collection('alarms').find({
+                source_username: item, 
+                sent: null
+            },{ projection: { _id: 0}}).toArray((err, res) => {
+                async function printCsv(data) {
+                    let csvdata = await new ObjectsToCsv(data).toString()
+                    
+
+                    
+                    let mailDetails = {
+	from: 'security <trojans@insideview.com>',
+	to: `${item}`,
+	cc: `trojans@insideview.com`,
+	subject: 'File shared outside IV over email',
+	html: `<html>
+<body>
+
+<p>As part of Security Monitoring, we observed that you have shared a few files which are mentioned in below attachment to the external user's email address</p>
+<br>
+<p>As part of our Security Policy, Can you answer the questions below ?
+</p>
+<p>1. Why is the transfer of files done to external email address ?
+</p>
+<p>2. Who is that recipient ? Customer or Personal
+</p>
+<p>3. What is that file and does it belong to IV ?
+</p>
+
+
+</body>
+</html>`,
+attachments: [
+      {
+        filename: "file.csv",
+        content: csvdata,
+      },
+    ]
+};
+
+mailTransporter.sendMail(mailDetails, function(err, data) {
+	if(err) {
+		console.log(err);
+	} else {
+		
+		db.collection('alarms').update({source_username: item}, {$set:{sent: 1}}, {multi:true}, (
+			err, res) => {
+			console.log('Email sent successfully ======>>>', item);
+			cb();
+		})
+	}
+});
+
+
+                }
+                printCsv(res);
+            })
+        }, (error, resp) => {
+        	console.log('============================');
+        })
+    })
+}
